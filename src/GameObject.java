eaae76838e04a2af832d182841e8bef718e434ce
mjfre <league@iMac.attlocal.net>
2018-07-13 14:53:57 -0700

    Power(IP)

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,6 +1,16 @@
+import java.awt.Graphics;
+
 public class GameObject {
 
 	public GameObject() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void draw(Graphics g) {
 		
 	}
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,6 +1,83 @@
-public class GamePanel {
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
+
+	final int MENU_STATE = 0;
+	final int GAME_STATE = 1;
+	final int END_STATE = 2;
+	int currentState = MENU_STATE;
 
 	public GamePanel() {
-		
+
 	}
+
+	public void startGame() {
+
+	} 
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (currentState == MENU_STATE) {
+			updateMenuState();
+		} else if (currentState == GAME_STATE) {
+			updateGameState();
+		} else if (currentState == END_STATE) {
+			updateEndState();
+		}
+		repaint();
+	}
+
+	public void paintComponent(Graphics g) {
+
+	}
+
+	public void updateMenuState() {
+
+	}
+
+	public void updateGameState() {
+
+	}
+
+	public void updateEndState() {
+
+	}
+
+	public void drawMenuState() {
+
+	}
+
+	public void drawGameState() {
+
+	}
+
+	public void drawEndState() {
+
+	}
+
 }

c1ceea179e861ca25a221876fea97825ab4ff143
Katelyn-Dragon <league@WTS20.attlocal.net>
2018-10-01 18:24:59 -0700

    Power(IP)

--- src/Humans.java
+++ src/Humans.java
@@ -12,9 +12,9 @@ public class Humans extends GameObject {
 	int walktimer = 0;
 	BufferedImage[] walk;
 	int walkIndex = 0;
-	
+
 	public static BufferedImage PCY1Img;
-	
+
 	public static BufferedImage PCY2Img;
 
 	public static BufferedImage PCY3Img;
@@ -27,27 +27,26 @@ public class Humans extends GameObject {
 
 	public static BufferedImage PCY7Img;
 
-
 	public Humans(int x, int y) {
 		super(x, y);
 		walk = new BufferedImage[7];
-		
+
 		try {
-			
-		walk[0] = ImageIO.read(this.getClass().getResourceAsStream("PCY1.png"));
-		
-		walk[1] = ImageIO.read(this.getClass().getResourceAsStream("PCY2.png"));
 
-		walk[2] = ImageIO.read(this.getClass().getResourceAsStream("PCY3.png"));
+			walk[0] = ImageIO.read(this.getClass().getResourceAsStream("PCY1.png"));
 
-		walk[3] = ImageIO.read(this.getClass().getResourceAsStream("PCY4.png"));
+			walk[1] = ImageIO.read(this.getClass().getResourceAsStream("PCY2.png"));
 
-		walk[4] = ImageIO.read(this.getClass().getResourceAsStream("PCY5.png"));
+			walk[2] = ImageIO.read(this.getClass().getResourceAsStream("PCY3.png"));
 
-		walk[5] = ImageIO.read(this.getClass().getResourceAsStream("PCY6.png"));
+			walk[3] = ImageIO.read(this.getClass().getResourceAsStream("PCY4.png"));
 
-		walk[6] = ImageIO.read(this.getClass().getResourceAsStream("PCY7.png"));
-		
+			walk[4] = ImageIO.read(this.getClass().getResourceAsStream("PCY5.png"));
+
+			walk[5] = ImageIO.read(this.getClass().getResourceAsStream("PCY6.png"));
+
+			walk[6] = ImageIO.read(this.getClass().getResourceAsStream("PCY7.png"));
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -60,44 +59,61 @@ public class Humans extends GameObject {
 		if (forwards == true) {
 			x += 10;
 		}
+		if (backwards == true) {
+			x-= 10;
+		}
 	}
 
 	public void draw(Graphics g) {
-		
-		if (isShooting == false && forwards == false) {
+
+		if (isShooting == false && forwards == false && backwards == false) {
 			g.drawImage(walk[0], x, y, null);
-		} 
-		else if (isShooting == true && forwards == false) {
+		} else if (isShooting == true && forwards == false && backwards == false) {
 			g.drawImage(GamePanel.ShootImg, x, y, null);
-		} 
-		else if (isShooting == false && forwards == true) {
+		} else if (isShooting == false && forwards == true && backwards == false) {
 			if (walktimer < 50) {
 				walkIndex = 0;
-			}
-			else if (walktimer < 100) {
+			} else if (walktimer < 100) {
 				walkIndex = 1;
-			}
-			else if (walktimer < 150) {
+			} else if (walktimer < 150) {
 				walkIndex = 2;
-			}
-			else if (walktimer < 200) {
-				walkIndex = 3;			}
-			else if(walktimer < 250) {
+			} else if (walktimer < 200) {
+				walkIndex = 3;
+			} else if (walktimer < 250) {
 				walkIndex = 4;
-			}
-			else if (walktimer < 300) {
+			} else if (walktimer < 300) {
 				walkIndex = 5;
-			}
-			else if (walktimer < 350) {
+			} else if (walktimer < 350) {
 				walkIndex = 6;
-			}
-			
-			else {
+			} else {
 				walktimer = 0;
-			} 
+			}
 			g.drawImage(walk[walkIndex], x, y, null);
 			walktimer++;
 		}
-
+		if (isShooting == false && forwards == false && backwards == true) {
+			if (walktimer < 50) {
+				walkIndex = 6;
+			} else if (walktimer < 100) {
+				walkIndex = 5;
+			} else if (walktimer < 150) {
+				walkIndex = 4;
+			} else if (walktimer < 200) {
+				walkIndex = 3;
+			} else if (walktimer < 250) {
+				walkIndex = 3;
+			} else if (walktimer < 300) {
+				walkIndex = 2;
+			} else if (walktimer < 350) {
+				walkIndex = 1;
+			} else if (walktimer < 400) {
+				walkIndex = 0;
+			} else {
+				walkIndex = 0;
+			}
+			g.drawImage(walk[walkIndex], x, y, null);
+			walktimer++;
+		}
 	}
+
 }

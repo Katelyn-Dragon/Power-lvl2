500fd2de2658a1a3c1d2da2cb005cc694e250559
11000783 <josephaguilar2022@gmail.com>
2018-11-26 18:21:06 -0800

    Power(IP)

--- src/Shot.java
+++ src/Shot.java
@@ -1,4 +1,17 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class Shot {
-
+public class Shot extends GameObject{
+	
+	public Shot (int x, int y) {
+		super(x,y);
+	}
+	public void update() {
+		
+	}
+	public void draw(Graphics g) {
+		g.setColor(Color.GREEN);
+		g.drawRect(x, y, 5, 1);
+		g.fillRect(x, y, 5, 1);
+	}
 }

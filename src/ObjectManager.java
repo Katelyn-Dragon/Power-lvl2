563549f3bb99c99440b8665f5dd6743890edb150
sepfish <40220408+sepfish@users.noreply.github.com>
2019-02-11 18:29:09 -0800

    Power(IP)

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -7,7 +7,6 @@ public class ObjectManager {
 	Enemy enemies;
 	Robot robot;
 	ArrayList <Shot> shots  = new ArrayList <Shot>();
-	ArrayList <Shot> shots2  = new ArrayList <Shot>();
 	
 	public ObjectManager(Humans human, Robot robots, Enemy enemy) {
 		humans = human;
@@ -22,21 +21,15 @@ public class ObjectManager {
 		for (int i = 0; i < shots.size(); i++) {
 			shots.get(i).update();
 		}
-		for (int i = 0; i < shots2.size(); i++) {
-			shots2.get(i).update();
-		}
 
 	}
 	
 	public void draw(Graphics g) {
 		humans.draw(g);
 		enemies.draw(g);
-		robot.draw(g);
+		
 		for (int i = 0; i < shots.size(); i++) {
 			shots.get(i).draw(g);
-		}
-		for (int i = 0; i < shots2.size(); i++) {
-			shots2.get(i).draw(g);
 		}
 		
 	}
@@ -44,11 +37,6 @@ public class ObjectManager {
 	public void playerShot() {
 		Shot shot = new Shot(humans.x + 240, humans.y + 150);
 		shots.add(shot);
-	}
-	
-	public void enemyShot() {
-		Shot shot2 = new Shot(enemies.x + 240, enemies.y + 150);
-		shots2.add(shot2);
 	}
 	
 	public void checkCollision() {
